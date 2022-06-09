28cfc44f21b6bf2b8fb95d191701947e5cb84bea
SamShanks1 <68856260+SamShanks1@users.noreply.github.com>
2022-06-09 17:47:07 +0100

    fix(playerload): data format

--- server.lua
+++ server.lua
@@ -1,35 +1,35 @@
-local QBCore = exports['qb-core']:GetCoreObject()
+local QBCore = exports["qb-core"]:GetCoreObject()
 
 local function addCash(src, amount)
-    local Player = QBCore.Functions.GetPlayer(src)
-    Player.Functions.AddMoney('cash', amount)
+	local Player = QBCore.Functions.GetPlayer(src)
+	Player.Functions.AddMoney("cash", amount)
 end
 
 local function removeCash(src, amount)
-    local Player = QBCore.Functions.GetPlayer(src)
-    Player.Functions.RemoveMoney('cash', amount)
+	local Player = QBCore.Functions.GetPlayer(src)
+	Player.Functions.RemoveMoney("cash", amount)
 end
 
 local function getCash(src)
-    local Player = QBCore.Functions.GetPlayer(src)
-    return Player.PlayerData.money["cash"] or 0
+	local Player = QBCore.Functions.GetPlayer(src)
+	return Player.PlayerData.money["cash"] or 0
 end
 
 exports("addCash", addCash)
 exports("removeCash", removeCash)
 exports("getCash", getCash)
 
-AddEventHandler('QBCore:Server:PlayerLoaded', function(player)
-    local citizenid = player.PlayerData.citizenid
-    local charInfo = player.PlayerData.charinfo
-    local playerSrc = player.PlayerData.source
-    exports.pefcl:loadPlayer({
-        source = playerSrc,
-        identifier = citizenid,
-        name = charInfo.firstname.." "..charInfo.lastname,
-      })
+AddEventHandler("QBCore:Server:PlayerLoaded", function(player)
+	local citizenid = player.PlayerData.citizenid
+	local charInfo = player.PlayerData.charinfo
+	local playerSrc = player.PlayerData.source
+	exports.pefcl:loadPlayer(playerSrc, {
+		source = playerSrc,
+		identifier = citizenid,
+		name = charInfo.firstname .. " " .. charInfo.lastname,
+	})
 end)
 
-AddEventHandler('QBCore:Client:OnPlayerUnload', function(src)
-    exports.pefcl:unloadPlayer(src)
+AddEventHandler("QBCore:Client:OnPlayerUnload", function(src)
+	exports.pefcl:unloadPlayer(src)
 end)
